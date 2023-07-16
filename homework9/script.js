f174f6bb1e7a3199dda8f987338c72a49c52a253
user8576285 <kompprogrammiryc@gmail.com>
2023-07-16 19:15:21 +0300

    >

--- homework9/script.js
+++ homework9/script.js
@@ -3,7 +3,7 @@ const users = ["user1", "user2", "user3", "user4", "user5", "user6"];
 const result = users.every((item) => item.length > 4);
 console.log(result);
 
-const result2 = users.some((item) => item.length < 4);
+const result2 = users.some((item) => item.length > 4);
 console.log(result2);
 
 const result3 = users.map(function (item) {
