41173f189d6e2537f623ab78da953b6f84a55b73
user8576285 <kompprogrammiryc@gmail.com>
2023-07-22 00:40:43 +0300

    homework13

---
+++ homework13/script.js
@@ -0,0 +1,6 @@
+const splitStringIntoArray = (str)  =>{
+    return str.split(" ");
+  }
+  
+  console.log(splitStringIntoArray("Robin Singh"));
+  console.log(splitStringIntoArray("I love arrays they are my favorite"));
