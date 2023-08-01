b6ef5ccbcebaa8233d66277134053096d7862369
user8576285 <kompprogrammiryc@gmail.com>
2023-08-01 16:50:56 +0300

    homework14

---
+++ homework14/script.js
@@ -0,0 +1,16 @@
+const number = +prompt("Введіть число між 0 та 3", "");
+switch (number) {
+    case 0:
+        alert("Ви ввели число 0");
+        break;
+    case 1:
+        alert("Ви ввели число 1");
+        break;
+    case 2:
+    case 3:
+        alert("Ви ввели число 2, може й 3");
+        break;
+    default:
+        alert("Ви ввели некоректне число");
+        break;
+}
