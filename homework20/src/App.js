d526e9bf5c155c6e4ee16064a0777ba536c88bdc
user8576285 <kompprogrammiryc@gmail.com>
2023-09-10 00:06:26 +0300

    homework20

---
+++ homework20/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <div className="App">Hello World</div>
+     <Button></Button>
+ 
+     
+    
+    </div>
+  );
+}
+
+export default App;
