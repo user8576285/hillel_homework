183674c54d738845de8a3953bea1f140d0a5ed78
user8576285 <kompprogrammiryc@gmail.com>
2023-09-19 17:30:24 +0300

    homework17

---
+++ homework17/index.js
@@ -0,0 +1,54 @@
+class Person {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  }
+  
+
+  class Apartment {
+    constructor() {
+      this.residents = [];
+    }
+  
+    addResident(person) {
+      this.residents.push(person);
+    }
+  }
+  
+  class House {
+    constructor(maxApartments) {
+      this.apartments = [];
+      this.maxApartments = maxApartments;
+    }
+  
+    addApartment(apartment) {
+      if (this.apartments.length < this.maxApartments) {
+        this.apartments.push(apartment);
+      } else {
+        console.log("Достигнуто максимальное количество квартир.");
+      }
+    }
+  }
+
+  
+
+const person1 = new Person("Никита","мужской");
+const person2 = new Person("Настя", "женский");
+
+
+const apartment1 = new Apartment();
+const apartment2 = new Apartment();
+
+
+apartment1.addResident(person1);
+apartment2.addResident(person2);
+
+
+const house = new House(2);
+
+
+house.addApartment(apartment1);
+house.addApartment(apartment2);
+
+console.log(house);
